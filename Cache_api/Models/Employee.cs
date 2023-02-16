fa3f6f993204389ce18a89459427a2b1e0030df3
mohammad <mrmy98@gmail.com>
2023-02-16 16:30:35 +0330

    Cache_api

---
+++ Cache_api/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Cache_api.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ Cache_api/Models/Repository/IDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Cache_api.Models.Repository
+{
+    public interface IDataRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(long id);
+        void Add(T entity);
+        void Update(T dbEntity, T entity);
+        void Delete(T entity);
+    }
+}
